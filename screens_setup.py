5ed0e9c02f39144bd06cfda1fe85d08a8aeb3f83
Kyle Rossi <kyle01nicholas@roadrunner.com>
2021-03-11 22:12:37 -0500

    Adjust function locations and add *json to .gitignore
    Functions were moved accordingly via Kien's suggestions and .json files should now be added to .gitignore so the sample .json files will no longer show up in the repository.

--- screens_setup.py
+++ screens_setup.py
@@ -8,54 +8,21 @@ a virtual environment with Kivy before using.
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
 
-from screen_1 import update_remaining_tasks
-from screen_2 import PostError, create_task
-
-# Lists of arrays of dictionaries that contain task entries.
-remaining_tasks = []
-completed_tasks = []
-
-# Default largest id for remaining_tasks.
-largest_id = 0
+from screen_2 import add_task_to_json, update_remaining_tasks
 
 
 class WindowOne(Screen):
-    def read_tasks_from_json(self):
-        update_remaining_tasks(self, remaining_tasks)
+    pass
 
 
 class WindowTwo(Screen):
-    def add_task_to_json(self):
-        global largest_id
-        current_largest_id = largest_id
+    def add_task(self):
+        add_task_to_json(self)
 
-        try:
-            new_task = create_task(self, current_largest_id)
-        except PostError as e:
-            error_text = str(e)
-
-            content = BoxLayout(orientation='vertical')
-            message_label = Label(text=error_text)
-            dismiss_button = Button(text='OK')
-
-            content.add_widget(message_label)
-            content.add_widget(dismiss_button)
-
-            popup = Popup(title='Error', content=content, size_hint=(0.5, 0.25))
-            dismiss_button.bind(on_press=popup.dismiss)
-            popup.open()
-        else:
-            remaining_tasks.append(new_task)
-            largest_id += 1
-
-            window_1_access = self.manager.get_screen('window_one')
-            window_1_access.read_tasks_from_json()
+    def update_tasks(self):
+        update_remaining_tasks(self)
 
 
 class WindowThree(Screen):
