1520a39a0d48c36fa51fe835bb8670bc44b7df3b
ducdao-git <ducdao.duke87@gmail.com>
2021-03-11 22:59:19 -0500

    add scrollable grid layout

--- screens/upcoming_route.py
+++ screens/upcoming_route.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
 # from kivy.core.window import Window
 from kivy.config import Config
 
