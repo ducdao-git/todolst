95573b01b7408b118b382c9a9aadf9c19b648bf5
KienLeWooster <kle22@wooster.edu>
2021-03-15 11:20:13 -0400

    Implement removing completed task.

--- screen_4.py
+++ screen_4.py
@@ -1,3 +1,5 @@
+import datetime
+
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -11,32 +13,60 @@ Functions used by WindowFour in screens_setups.py can be found here.
 
 
 class Row(BoxLayout):
-    def remove(self):
-        self.parent.remove_widget(self)
+    def __init__(self, id, **kwargs):
+        super().__init__(**kwargs)
+        self.id = id
 
 
 class WindowFour(Screen):
-    data = [{"subject": "Potato 1", "time": "Time 1"},
-            {"subject": "Potato 2", "time": "Time 1"},
-            {"subject": "Potato 3", "time": "Time 1"},
-            {"subject": "Potato 4", "time": "Time 1"},
-            {"subject": "Potato 5", "time": "Time 1"},
-            {"subject": "Potato 6", "time": "Time 1"},
-            {"subject": "Potato 7", "time": "Time 1"},
-            {"subject": "Potato 8", "time": "Time 1"},
-            {"subject": "Potato 9", "time": "Time 1"},
-            {"subject": "Potato 10", "time": "Time 1"},
-            {"subject": "Potato 11", "time": "Time 1"},
-            {"subject": "Potato 12", "time": "Time 1"},
-            {"subject": "Potato 13", "time": "Time 1"}]
+    data = [{'id': 1, 'subject': 'cs230 hw1', 'time': '2021-03-11 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 1, 10, 30)},
+            {'id': 2, 'subject': 'cs230 hw2', 'time': '2021-03-12 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 2, 10, 30)},
+            {'id': 3, 'subject': 'cs230 hw3', 'time': '2021-03-13 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 3, 10, 30)},
+            {'id': 4, 'subject': 'cs230 hw4', 'time': '2021-03-14 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 4, 10, 30)},
+            {'id': 5, 'subject': 'cs230 hw5', 'time': '2021-03-15 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 5, 10, 30)},
+            {'id': 6, 'subject': 'cs230 hw6', 'time': '2021-03-16 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 6, 10, 30)},
+            {'id': 7, 'subject': 'cs230 hw7', 'time': '2021-03-17 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 7, 10, 30)},
+            {'id': 8, 'subject': 'cs230 hw8', 'time': '2021-03-18 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 8, 10, 30)},
+            {'id': 9, 'subject': 'cs230 hw9', 'time': '2021-03-19 23:59',
+             'priority': 'none',
+             'completed_time': datetime.datetime(2021, 3, 9, 10, 30)}
+            ]
 
     def on_enter(self, *args):
         self.populate_tasks(self.data)
 
+    def on_leave(self):
+        self.ids.completed_tasks.clear_widgets()
+
     def populate_tasks(self, data):
+        def remove_row(*args):
+            pressed_button = args[0]
+            for task in self.data:
+                if task['id'] == pressed_button.parent.id:
+                    self.data.remove(task)
+                    break
+            self.ids.completed_tasks.remove_widget(pressed_button.parent)
+
         for i in data:
-            row = Row()
+            row = Row(i['id'])
 
             row.ids.task_label.text = f'Subject: {i["subject"]}\nTime:{i["time"]}'
+            row.ids.remove_button.bind(on_release=remove_row)
 
             self.ids.completed_tasks.add_widget(row)
