cbd38fca8318cbbdc4439561637ef51c92c3975f
ducdao-git <ducdao.duke87@gmail.com>
2021-03-18 00:10:40 -0400

    fix AddTaskRoute logic
    - add datetime input validation for rich option like tmr, today, mar 20, etc.
    - input validation is part of _create_due_time func
    - add interaction between AddTaskRoute and process_task_handler

--- libs/data_handler.py
+++ libs/data_handler.py
@@ -1,6 +1,11 @@
+from libs.get_data import add_item_to_dict
+from datetime import datetime
+
+
 class ProcessTaskHandler:
     def __init__(self, app, _to, task):
-        self.user_data = app.user_data
+        self.upcoming_tasks = app.user_data['upcoming']
+        self.completed_tasks = app.user_data['completed']
         self.task = task
 
         if _to == 'upcoming':
@@ -11,7 +16,15 @@ class ProcessTaskHandler:
             raise ValueError()
 
     def add_upcoming_task(self):
+        if "completed_time" in self.task.keys():
+            del self.task["completed_time"]
+
+        date, time = self.task["time"].split()
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+        self.task["time"] = time
+
         print(self.task)
+        add_item_to_dict(self.upcoming_tasks['on_time'], date, self.task)
 
     def add_completed_task(self):
         print(self.task)
--- screens/add_task_route.py
+++ screens/add_task_route.py
@@ -1,20 +1,51 @@
-from datetime import datetime
+import datetime
+from dateutil import parser
+
 from kivy.uix.screenmanager import Screen
 
 
-# def _datetime_validation(datetime_str):
-#     """
-#     check validation of datetime
-#     :pram datetime_str: string representation of a datetime
-#     :return: True if datetime is valid, else False
-#     """
-#     datetime_str = datetime_str.split()
-#     # if datetime_str
-
-
-# Custom Exception used to catch errors in create_task().
-class InvalidTimeError(Exception):
+# Custom Exception used to catch errors in add_task_to_upcoming().
+class AddTaskError(Exception):
     pass
+
+
+def _create_due_time(datetime_rep):
+    """
+    create and format to a valid string represent due datetime
+    :pram datetime_rep: string representation of a datetime
+    :return: string represent datetime in form Year-month-date Hour:Minute
+    """
+    if not datetime_rep:
+        return str(datetime.date.today()) + " 23:59"
+
+    datetime_rep = datetime_rep.lower()
+    datetime_lst = datetime_rep.split()
+
+    if len(datetime_lst) == 1:
+        datetime_lst.append('23:59')
+
+    for i in range(len(datetime_lst)):
+
+        if datetime_lst[i] in ['tdy', '2day', '2de', '2da', '2d', 'today']:
+            datetime_lst[i] = str(datetime.date.today())
+            break
+
+        elif datetime_lst[i] in ['tomw', 'tmw', 'tmr', '2moro', 'tomorrow']:
+            datetime_lst[i] = str(
+                datetime.date.today() + datetime.timedelta(days=1))
+            break
+
+    datetime_rep = ' '.join(datetime_lst)
+
+    try:
+        datetime_rep = parser.parse(datetime_rep)
+    except ValueError or OverflowError as e:
+        raise AddTaskError(e)
+
+    if datetime_rep < datetime.datetime.today():
+        raise AddTaskError("Due time can't smaller than current time")
+    else:
+        return datetime_rep.strftime('%Y-%m-%d %H:%M')
 
 
 class AddTaskRoute(Screen):
@@ -23,32 +54,29 @@ class AddTaskRoute(Screen):
         self.app = app
         self.largest_id = app.user_data['largest_id']
 
-    # def create_task(self):
-    #     """
-    #     turns the task information the user inputs into a dictionary
-    #     that can be transferred back and displayed on UpcomingRoute.
-    #
-    #     :return: The task in dictionary form, task_as_dict
-    #     """
-    #     if self.ids.body.text == "":
-    #         error = 'Make sure the Body field is provided.'
-    #         raise InvalidTimeError(error)
-    #
-    #     task_as_dict = {
-    #         "id": self.largest_id,
-    #         "body": self.ids.body.text,
-    #         "time": self.ids.time.text,
-    #         "priority": self.ids.priority.active,
-    #         "status": 0
-    #     }
-    #
-    #     return task_as_dict
-    #
-    # def _set_due_time(self):
-    #     input_time = self.ids.time.text
-    #     curr_time = datetime.now()
-    #
-    #     if not len(input_time):
-    #         return curr_time.strftime("%Y-%m-%d %H:%M")
-    #     else:
-    #         _datetime_validation(input_time)
+    def add_task_to_upcoming(self):
+        """
+        turns the task information the user inputs into a dictionary
+        that can be transferred back and displayed on UpcomingRoute.
+
+        :return: The task in dictionary form, task_as_dict
+        """
+        if not self.ids.subject.text and self.ids.time.text:
+            error = 'Missing task description'
+            raise AddTaskError(error)
+        elif not self.ids.subject.text and not self.ids.time.text:
+            return
+
+        self.largest_id += 1
+
+        new_task = {
+            "id": self.largest_id,
+            "subject": self.ids.subject.text,
+            "time": _create_due_time(self.ids.time.text),
+            "priority": "none"
+        }
+
+        self.ids.subject.subject = ''
+        self.ids.subject.text = ''
+
+        self.app.process_task_handler('upcoming', new_task)
