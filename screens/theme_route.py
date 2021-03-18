1318f2ed9504148fba4c0a485e8d54e08bc5a7c8
KienLeWooster <kle22@wooster.edu>
2021-03-18 18:09:09 -0400

    Implement switching theme in interface.

--- screens/theme_route.py
+++ screens/theme_route.py
@@ -13,8 +13,10 @@ Functions used by WindowFour in screens_setups.py can be found here.
 
 
 class ThemeLayout(BoxLayout):
-    def __init__(self, theme_name, theme_dict, **kwargs):
+    def __init__(self, root, theme_name, theme_dict, **kwargs):
         super().__init__(**kwargs)
+        self.root = root
+        self.on_touch_up_ran = True
         self.primary_color_untouched = theme_dict['primary_color']
         self.primary_text_color_untouched = theme_dict['text_primary_color']
         self.secondary_color_untouched = theme_dict['secondary_color']
@@ -29,13 +31,13 @@ class ThemeLayout(BoxLayout):
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            self.ids.primary_color_label_button.background_color = self.touched_color
-            self.ids.space_filling_label_button.background_color = self.touched_color
+            self.ids.overlay.opacity = 0.5
+            self.ids.choice_checkbox.active = True
 
     def on_touch_up(self, touch):
         self.ids.primary_color_label_button.background_color = self.primary_color_untouched
-        self.ids.choice_checkbox.active = True
         self.ids.space_filling_label_button.background_color = self.theme_background_color
+        self.ids.overlay.opacity = 1
 
 
 class ThemeRoute(Screen):
@@ -44,10 +46,10 @@ class ThemeRoute(Screen):
         self.app = app
         self.normal_theme = get_theme_palette("todolst")
         self.dark_theme = get_theme_palette("dark")
-        self.chosen_theme_checkbox = 0
 
     def add_theme_choice(self, theme_name, theme_dict):
-        theme_choice = ThemeLayout(theme_name, theme_dict)
+        theme_choice = ThemeLayout(self, theme_name, theme_dict)
+        theme_choice.ids.choice_checkbox.text = theme_name
         self.ids.themes.add_widget(theme_choice)
 
     def on_pre_enter(self, *args):
